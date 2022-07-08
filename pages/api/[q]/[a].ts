27df742564520238f934a11bee99b71b7aa75e9a
Liuming Chen <51524534+let-lc@users.noreply.github.com>
2022-07-08 00:31:00 -0400

    refactor: wrap API functions with middleware

--- pages/api/[q]/[a].ts
+++ pages/api/[q]/[a].ts
@@ -1,15 +1,3 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import answers from "../../../data/answers.json";
+import { checkAnswerMiddleware } from '@/libs/api-middleware';
 
-const CheckAnswer = (req: NextApiRequest, res: NextApiResponse) => {
-  const q = req.query?.q as string;
-  const a = req.query?.a as string;
-
-  if (!q) return res.status(400).send("Question number is required.");
-  if (!a) return res.status(400).send("Answer is required.");
-  if (answers[q] === undefined) return res.status(200).send("?");
-
-  res.status(200).send(String(answers[q]) === a ? 1 : 0);
-};
-
-export default CheckAnswer;
+export default checkAnswerMiddleware(() => {});
