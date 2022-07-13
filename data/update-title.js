0053bbfdeb0a8a31885b6da56bf2ec0dedaf4b86
Liuming Chen <51524534+let-lc@users.noreply.github.com>
2022-07-12 23:13:59 -0400

    refactor: replace with project the new titles API

--- data/update-title.js
+++ data/update-title.js
@@ -4,15 +4,21 @@ const { parse } = require('csv-parse/sync');
 const { default: axios } = require('axios');
 
 // Download titles
-axios.get('https://projecteuler.net/minimal=problems;csv').then((res) => {
-  fs.writeFileSync(
-    path.join(__dirname, 'titles.json'),
-    JSON.stringify(
-      parse(res.data, { columns: true, skip_empty_lines: true }).map((row) => ({
-        id: Number(row.ID),
-        published: Number(row.Published),
-        title: row.Description,
-      }))
-    )
-  );
-});
+axios
+  .get('https://euler.haku.dev/api/titles', {
+    params: { token: process.env.TOKEN },
+  })
+  .then((res) => {
+    fs.writeFileSync(
+      path.join(__dirname, 'titles.json'),
+      JSON.stringify(
+        parse(res.data, { columns: true, skip_empty_lines: true }).map(
+          (row) => ({
+            id: Number(row.ID),
+            published: Number(row.Published),
+            title: row.Description,
+          })
+        )
+      )
+    );
+  });
